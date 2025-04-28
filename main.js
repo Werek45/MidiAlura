9b8150300d18e50aa58b4c32536b969d71b5d943
Werek Silva <github.com/Werek45>
2025-04-28 16:20:26 -0300

    change tocaSomPom for tocaSom(idElementoaudio)

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-// Pom ----------------------------------------
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let cont = 0;
+
+while (cont < listaDeTeclas.length) {
+    listaDeTeclas[cont].onclick = tocaSom;
+    cont = cont + 1;
+}
